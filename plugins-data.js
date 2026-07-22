2a07fab964a58b656abb1de686dec267603b385c
kazuya-wings <kazuya631120@gmail.com>
2026-07-22 21:24:15 +0900

    誤字修正

--- plugins-data.js
+++ plugins-data.js
@@ -1,7 +1,7 @@
 const plugins = [
   {
     id: "plugin-1",
-    name: "編集時自動ルックアッ",
+    name: "編集時自動ルックアップ",
     icon: "🚀",
     description: "レコードの作成・編集画面を開くだけで、指定したルックアップフィールドを自動で取得します。",
     page: "plugins/plugin-1.html"
